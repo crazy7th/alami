adcc74cc0cde59d137999da060fc441514214af7
Ekky Patria <ekky@alterra.id>
2022-07-22 17:47:27 +0700

    move some static data to constants

--- src/components/home/heroImage.jsx
+++ src/components/home/heroImage.jsx
@@ -1,29 +1,31 @@
 import { SuperGraph } from './superGraph';
+import {
+    Hero
+} from '../../constants/data';
 
 export const HeroImage = () => {
     return (
         <div
             id="heroImage"
-            className="kt-hero-img bg-center md:bg-top text-white relative md:h-screen flex flex-col md:overflow-hidden"
+            className={`bg-[url('../public/img/8LxEvb4kU-unsplash.jpg')] bg-cover bg-center md:bg-top text-white relative md:h-screen flex flex-col md:overflow-hidden`}
         >
             <div className="h-full w-full bg-black/60">
                 <div className="container mx-auto text-center p-4 pt-20 mb-24">
                     <p>
-                        Catering should be an experience
+                        {Hero.tagline}
                     </p>
                     <h1 className="text-4xl md:text-6xl font-bold md:w-2/3 mx-auto my-5">
-                        We use only the finest and freshnest ingredients
+                        {Hero.headline}
                     </h1>
                     <p className="md:w-2/5 mx-auto my-10">
-                        At sway catering we know that food is an important of life.
-                        If the meal is not perfect, your event cannot be perfect.
+                        {Hero.description}
                     </p>
                     <div>
                         <a
                             href="#"
                             className="inline-block bg-green-500 border-2 border-green-500 text-white rounded-full font-bold px-8 py-3 hover:bg-white hover:text-green-500"
                         >
-                            Request a Quote ›
+                            {Hero.cta} ›
                         </a>
                     </div>
                 </div>
---
+++ src/constants/data.jsx
@@ -0,0 +1,33 @@
+export const Hero = {
+    tagline: 'Catering should be an experience',
+    headline: 'We use only the finest and freshnest ingredients',
+    description: 'At sway catering we know that food is an important of life. If the meal is not perfect, your event cannot be perfect.',
+    cta: 'Request a Quote'
+}
+
+export const PortofolioFeature = [
+    {
+        id: 1,
+        icon: 'x',
+        title: 'Design',
+        description: 'z'
+    },
+    {
+        id: 2,
+        icon: 'x',
+        title: 'Develop',
+        description: 'z'
+    },
+    {
+        id: 3,
+        icon: 'x',
+        title: 'Write',
+        description: 'z'
+    },
+    {
+        id: 4,
+        icon: 'x',
+        title: 'Promote',
+        description: 'z'
+    }
+];
